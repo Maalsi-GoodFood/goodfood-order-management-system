ad0b9938a72ef9c999b10d2f7874cee0f16ec30b
Stan <stan.reynaud@etu.univ-amu.fr>
2022-08-06 16:24:02 +0200

    feat: set dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
-FROM openjdk:8-jdk-alpine
-ENTRYPOINT ["java"]
+FROM openjdk:latest
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/com/goodfood/order_management_system/OrderManagementSystemApplicationTests.java
+++ src/test/java/com/goodfood/order_management_system/OrderManagementSystemApplicationTests.java
@@ -1,13 +1,9 @@
 package com.goodfood.order_management_system;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderManagementSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class
+OrderManagementSystemApplicationTests {
 
 }
